206a001e1a6a22cea5bae836795d69f6122f03f0
Borysevych <borysevych.rus@gmail.com>
2023-03-08 01:16:06 +0200

    make HW 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkUp(galleryItems);
 
-console.log(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItemsMarkUp(galleryItems) {
+    
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+        `;
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionsDelay: 250 });
